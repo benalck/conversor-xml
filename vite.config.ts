4831d95ad94ea8725b7dfeace04f078f55a926a7
benalck <nairton.neves12@gmail.com>
2025-03-10 20:59:11 -0300

    Adiciona GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/conversor-xml/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
